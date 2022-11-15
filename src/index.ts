9cfae47b01352b3b53272b34caee02a5767e277e
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-11-15 11:36:28 +0100

    chore(exports): :sparkles: export propreties

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Precondition } from './structures/Precondition';
+
+export { AmethystClient } from './structures/AmethystClient';
+export { AmethystCommand } from './structures/Command';
+export { Precondition } from './structures/Precondition';
+export { AmethystEvent } from './structures/Event';
+
+export const preconditions: Record<string, Precondition> = {
+    
+}
