1da0caf9f811cc6690f0a837623ce177153c576f
Greensky <david.hesliere@gmail.com>
2022-11-15 17:34:03 +0100

    chore(exports): Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
+// Imports
 import { Precondition } from './structures/Precondition';
+import DMOnly from './preconditions/DMOnly';
+import GuildOnly from './preconditions/GuildOnly';
+import NsfwOnly from './preconditions/NsfwOnly';
 
+// Structures
 export { AmethystClient } from './structures/AmethystClient';
 export { AmethystCommand } from './structures/Command';
 export { Precondition } from './structures/Precondition';
 export { AmethystEvent } from './structures/Event';
 
-import DMOnly from './preconditions/DMOnly';
-import GuildOnly from './preconditions/GuildOnly';
-import NsfwOnly from './preconditions/NsfwOnly';
+// Types
+export { PreconditionChatInputRun, preconditionRunReturn, PreconditionMessageRun } from './typings/Precondition'
+export { startOptions, AmethystClientOptions, commandDeniedCode, CommandType, errorCode, errorReason, deniedReason } from './typings/Client';
+export { ChatInputRun, commandDeniedPayload, commandOptions, MessageRun } from './typings/Command';
 
+// Objects
 export const preconditions: Record<string, Precondition> = {
     DMOnly,
     GuildOnly,
