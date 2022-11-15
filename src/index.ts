a74c45a8f0095e480eef61d75d6a797c38ab9b58
Greensky <david.hesliere@gmail.com>
2022-11-15 18:15:33 +0100

    chore(exports): Update exports

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,13 @@ export { AmethystClient } from './structures/AmethystClient';
 export { AmethystCommand } from './structures/Command';
 export { Precondition } from './structures/Precondition';
 export { AmethystEvent } from './structures/Event';
+export { AutocompleteListener } from './structures/AutocompleteListener';
 
 // Types
 export { PreconditionChatInputRun, preconditionRunReturn, PreconditionMessageRun } from './typings/Precondition'
 export { startOptions, AmethystClientOptions, commandDeniedCode, CommandType, errorCode, errorReason, deniedReason } from './typings/Client';
 export { ChatInputRun, commandDeniedPayload, commandOptions, MessageRun } from './typings/Command';
+export { AutocompleteListenerOptions, AutocompleteListenerRun } from './typings/autocompleteListener';
 
 // Objects
 export const preconditions: Record<string, Precondition> = {
