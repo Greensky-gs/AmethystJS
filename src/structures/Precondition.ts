3ab87b3ee263717a4a8c3bc098b52cbe34968ff9
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-11-17 15:33:13 +0100

    fix: :ambulance: Fix precondition usage

--- src/structures/Precondition.ts
+++ src/structures/Precondition.ts
@@ -2,24 +2,18 @@ import { PreconditionChatInputRun, PreconditionMessageRun } from '../typings/Pre
 
 export class Precondition {
     public readonly name: string;
-    private _chatInputRun: PreconditionChatInputRun;
-    private _messageRun: PreconditionMessageRun;
+    public chatInputRun: PreconditionChatInputRun;
+    public messageRun: PreconditionMessageRun;
 
     constructor(name: string) {
         this.name = name;
     }
     public setChatInputRun(run: PreconditionChatInputRun) {
-        this._chatInputRun = run;
+        this.chatInputRun = run;
         return this;
     }
     public setMessageRun(run: PreconditionMessageRun) {
-        this._messageRun = run;
+        this.messageRun = run;
         return this;
-    }
-    public get chatInputRun(): PreconditionChatInputRun {
-        return this._chatInputRun;
-    }
-    public get messageRun(): PreconditionMessageRun {
-        return this._messageRun;
     }
 }
