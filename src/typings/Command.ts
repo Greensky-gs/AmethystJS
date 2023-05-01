a9541b07a6940e5d429d9deab06e41c569d8fa04
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2023-05-01 09:11:05 +0200

    chore: :memo: Start doc in package

--- src/typings/Command.ts
+++ src/typings/Command.ts
@@ -10,9 +10,52 @@ import { AmethystCommand } from '../structures/Command';
 import { Precondition } from '../structures/Precondition';
 
 export type commandOptions = {
+    /**
+     * Cooldown time in seconds of the command
+     * 
+     * Users will have to wait `<#AmethystCommand>.options.cooldown` seconds before use the command again
+     * @event `commandDenied` When a command is denied by cooldown, the event `commandDenied` will be emited. The metadata object will contain `{ remainingCooldownTime: number }` as the remaining time in milliseconds
+     * @default Default value is the same as `<#AmethystClient>.options.defaultCooldownTime`
+     */
     cooldown?: number;
+    /**
+     * These are the permissions required for the user to use the command in a server
+     * It is an array containing `PermissionsString` from discord.js
+     * 
+     * ```js
+     * new AmethystCommand({
+     *     permissions: [ "ManageChannels", "BanMembers" ]
+     * });
+     * ```
+     * @event `commandDenied` When a command is denied by ermissions, the `commandDenied` event is emitted and the metadata will be `{ need: PermissionsString[]; got: PermissionsString[]; missing: PermissionsString[] }`
+     */
     permissions?: PermissionsString[];
+    /**
+     * These are the permissions required for the client to use the command in a server.
+     * Use this proprety to tell users what permissions your bot needs
+     * It is an array containing `PermissionsString` from discord.js
+     * 
+     * ```js
+     * new AmethystCommand({
+     *     clientPermissions: [ 'BannMembers', 'ViewAuditLogs' ]
+     * });
+     * ```
+     * @event `commandDenied` When a command is denied by clientPermissions, the `commandDenied` event is emitted and the metadata will be `{ need: PermissionsString[]; got: PermissionsString[]; missing: PermissionsString[] }`
+     */
     clientPermissions?: PermissionsString[];
+    /**
+     * These are the preconditions required by the command before running
+     * You can use your own preconditions from the preconditions folder or the default preconditions provided by AmethystJS
+     * 
+     * ```js
+     * const { preconditions } = require('amethystjs');
+     * const staffOnly = require('../preconditions/staffOnly');
+     * new AmethystCommand({
+     *     preconditions: [ preconditions.GuildOnly, staffOnly ]
+     * });
+     * ```
+     * @event `commandDenied` When a command is denied by precondition, the `commandDenied` event is emitted and the metadata depends of your precondition. If you used a precondi
+     */
     preconditions?: Precondition[];
     messageInputChannelTypes?: ChannelType[];
     aliases?: string[];
